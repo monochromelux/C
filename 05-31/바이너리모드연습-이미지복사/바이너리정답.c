dbfb4779ffd529ad708e5f7f7cc817a403c0a7cc
Hyoeun Lee <ggrr12345@naver.com>
2017-05-31 19:59:27 +0900

    Add files via upload

---
+++ 05-31/바이너리모드연습-이미지복사/바이너리정답.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <conio.h>
+
+main()
+{
+	
+	FILE *fp1, *fp2;
+	int c ;
+
+    
+    if ((fp1 = fopen("org_img.jpg", "rb")) == NULL ){
+		printf("file open error\n");
+		exit(0);
+	}
+	if ((fp2 = fopen("copy_img.jpg", "wb")) == NULL ){
+		printf("file open error\n");
+		exit(0);
+	}
+	while(1){
+		if( feof(fp1) != 0 ) break;
+		c = fgetc(fp1);
+	//	if ( c == EOF ) break;
+		fputc( c, fp2 );
+	}
+	fclose(fp1);
+	fclose(fp2);
+}
