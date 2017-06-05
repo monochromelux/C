8bd66d9abe7701505fb15a0c88aa190d155cd35a
Hyoeun Lee <ggrr12345@naver.com>
2017-06-05 21:04:05 +0900

    Add files via upload

--- 06-05/No.3.c
+++ 06-05/No.3.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void move() {
-	
-}
-
 int main(int argc, char *argv[]) {
-	
+	int i, j, k;
+	double a;
+	while(1) {
+		for(i = 0; i<40; i++) {
+			printf("%s", str);
+			for(k=1; k<6000000; k++) {
+				a = a*1.1-0.1*1.1;
+			}
+			for(j=0; j<strlen; j++) {
+				putchar('\b');
+			}
+			putchar(' ');
+		}
+		
+	}
 	
 	
 	return 0;
